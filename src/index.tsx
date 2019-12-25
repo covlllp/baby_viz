7027f9751d9a539a1cd941e77491c803b6104712
Colin VanLang <colinvanlang@gmail.com>
2019-12-24 19:17:20 -0500

    seed files for ts client

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
+// import { devToolsEnhancer } from 'redux-devtools-extension';
+
+// import { reducer } from 'data/reducer';
+
+// import { App } from 'container/app';
+
+// const store = createStore(reducer, devToolsEnhancer({}));
+
+render(<div />, document.getElementById('react-content'));
+
+//  <Provider store={store}>
+//    <App />
+//  </Provider>,
